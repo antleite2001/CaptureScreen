41f0fa74b015d84b8bcda95bd119edba1a04c4fc
acleite <antleite2001@gmail.com>
2019-03-05 07:53:28 +0000

    release 8

--- ni/Properties/Settings.Designer.cs
+++ ni/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace ni.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -56,6 +56,18 @@ namespace ni.Properties {
             }
             set {
                 this["KeepNumLockAlwaysOn"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool KeepCapsLockAlwaysOff {
+            get {
+                return ((bool)(this["KeepCapsLockAlwaysOff"]));
+            }
+            set {
+                this["KeepCapsLockAlwaysOff"] = value;
             }
         }
     }
